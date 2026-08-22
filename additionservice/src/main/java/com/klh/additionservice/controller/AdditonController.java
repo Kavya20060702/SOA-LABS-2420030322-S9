a8b405ffe923ffdeb8991a13c36d9e31be0301bc
P Kavya Sai <2420030322@klh.edu.in>
2026-08-22 12:16:13 +0530

    Add server port configuration to use environment variable in application.properties and update AdditonController to display port in response

--- additionservice/src/main/java/com/klh/additionservice/controller/AdditonController.java
+++ additionservice/src/main/java/com/klh/additionservice/controller/AdditonController.java
@@ -1,17 +1,19 @@
 package com.klh.additionservice.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class AdditonController 
-{
-        @GetMapping("/add")
-        public String add(@RequestParam int a, @RequestParam int b) 
-        {
-            int result = a + b;
-            return "Addition = " + result;
-        }
-}
+public class AdditonController {
 
+    @Value("${server.port}")
+    private String port;
+
+    @GetMapping("/add")
+    public String add(@RequestParam int a, @RequestParam int b) {
+        int result = a + b;
+        return "Addition = " + result + " from Port " + port;
+    }
+}
